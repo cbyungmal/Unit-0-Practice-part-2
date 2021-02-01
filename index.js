a4978e5c5247724c61e69bab8706c6d28036f2e0
Jonathan Corley <jcorley@noscihigh.org>
2021-02-01 21:06:01 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,109 @@
+//Unit 0 Practice Part 2
+
+/* 1.
+
+Write a function that returns the largest of
+two numbers.
+
+Example: 
+greaterThan(20, 4) => 20
+greaterThan(5, 15) => 15
+
+*/
+
+
+
+
+
+
+
+
+
+/* 2.
+
+Write a function that returns the smallest of
+two numbers.
+
+Example: 
+lessThan(20, 4) => 20
+lessThan(5, 15) => 15
+
+*/
+
+
+
+
+
+
+
+
+/* 3.
+
+Write a function that returns all of the values within a given array.
+
+Example: 
+
+var myArray = [0,1,2,3,4,5]
+
+arrayPrint(myArray) => 0 1 2 3 4 5
+arrayPrint(['hello', true]) => 'hello'  true
+
+*/
+
+
+
+
+
+
+
+
+/* 4.
+
+Write a function that prints the numbers from 0 to an input number.
+
+Example: 
+counter(6) => 0 1 2 3 4 5 6
+counter(10) => 0 1 2 3 4 5 6 7 8 9 10
+
+*/
+
+
+
+
+
+
+
+
+
+/* 5.
+
+Write a function that determines if a given input 
+number is even or odd
+
+Example: 
+isEven(6) => even
+isEven(55) => odd
+
+*/
+
+
+
+
+
+
+
+
+
+/* 5.
+
+Write a function that determines if a the numbers 
+in a given array are even or odd
+
+Example: 
+
+var numArray = 
+
+isEven(6) => even
+isEven(55) => odd
+
+*/
